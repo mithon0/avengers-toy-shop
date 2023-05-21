2283f1408cb2ad6334970c5e30d98eb8b309758e
mithon0 <mithonhossen7244@gmail.com>
2023-05-21 12:39:32 +0600

    Footer edite add social media button

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,5 +1,5 @@
-
-
+import { FaFacebookSquare,FaInstagramSquare } from "react-icons/fa";
+import logo from '../assets/images/logo.png'
 const Footer = () => {
     return (
         <footer className="footer  footer-center p-10 bg-slate-900 text-white  rounded">
@@ -10,13 +10,15 @@ const Footer = () => {
                 <a className="link link-hover">Press kit</a>
             </div>
             <div>
-                <div className="grid grid-flow-col gap-4">
+                <div className=" ">
                     
-                   
+                   <img className='w-14 mx-auto' src={logo} alt="" /><br />
+                   <button className='btn btn-outline btn-primary w-72'><FaFacebookSquare className="text-3xl"/> Contact us with Facebook</button>
+                   <button  className='btn btn-outline btn-error w-72'><FaInstagramSquare className="text-3xl"/> Contact us with Instagram</button>
                 </div>
             </div>
             <div>
-                <p>Copyright © 2023 - All right reserved by <span className="font-bold">My Toy<span className="text-red-600">Store</span></span> Ltd</p>
+                <p>Copyright © 2023 - All right reserved by <span className="font-bold">Avengers Toy<span className="text-red-600">Store</span></span> Ltd</p>
             </div>
         </footer>
     );
