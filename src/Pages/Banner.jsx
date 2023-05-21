3a3c5fc6adc881e86234d8915cb4df6f6559c806
mithon0 <mithonhossen7244@gmail.com>
2023-05-21 11:48:12 +0600

    update banner text

--- src/Pages/Banner.jsx
+++ src/Pages/Banner.jsx
@@ -5,12 +5,13 @@ const Banner = () => {
         <div className="relative w-full my-10 ">
            <img src={img} className="w-full  rounded-lg" alt="" /> 
            <div className="absolute top-0 flex w-full bg-gradient-to-r from-[#151515] to-[#15151500]  transform h-[520px] rounded-lg ">
-            <div className='w-2/3 m-10 mt-36'>
-            <h1 className='title mb-4 text-4xl font-bold text-blue-500 mt-5 '>Avengers Toys</h1>
-            <p className=' text-blue-400'>
+            <div className='w-2/3  m-10 mx-auto text-center'>
+                <h1 className='text-8xl text-red-100'>WEllCOME to AVENGER-TOY STORE</h1>
+            
+            <p className=' text-red-100'>
             Kids love for superheroes is unexplainable. More often than not, kids love to imitate their favourite superheroes. Its okay for kids to love these characters as they preach goodness and kindness in the world. If your kid is an Avengers fan, you can give him/her some Avengers toys that will surely put them on cloud 9. Online stores have Avengers Endgame toys, Marvel Avengers toys, Thanos toys, Iron Man toys, Hulk toys
             </p>
-            <button className='btn mt-5 btn-primary  px-4 w-40 '>register</button>
+            <button className='btn mt-5 btn-primary  px-4 w-40 '>GetStarted</button>
             </div>
            </div>
         </div>
